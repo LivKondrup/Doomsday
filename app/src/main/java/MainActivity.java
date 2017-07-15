ef42e0b17ab6faa01c447f149e6091798d663f3d
Liv Kondrup <kondrup.liv@gmail.com>
2017-07-16 00:39:19 +0200

    Start

---
+++ app/src/main/java/MainActivity.java
@@ -0,0 +1,18 @@
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.example.livkondrup.doomsday.R;
+
+/**
+ * Created by Liv Kondrup on 16-07-2017.
+ */
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
